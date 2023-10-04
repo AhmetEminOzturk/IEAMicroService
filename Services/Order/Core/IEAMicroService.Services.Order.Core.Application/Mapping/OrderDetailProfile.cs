5a5ba9e46819cc767308d5495145d2221cb3ccc1
AhmetEminOzturk <eminahmetozturk@gmail.com>
2023-10-04 15:56:51 +0300

    MediatR-Onion

---
+++ Services/Order/Core/IEAMicroService.Services.Order.Core.Application/Mapping/OrderDetailProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using IEAMicroService.Services.Order.Core.Application.Dtos.AddressDtos;
+using IEAMicroService.Services.Order.Core.Application.Dtos.OrderDetailDtos;
+using IEAMicroService.Services.Order.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEAMicroService.Services.Order.Core.Application.Mapping
+{
+    public class OrderDetailProfile : Profile
+    {
+        public OrderDetailProfile()
+        {
+            CreateMap<ResultOrderDetailDto, OrderDetail>().ReverseMap();
+            CreateMap<CreateOrderDetailDto, OrderDetail>().ReverseMap();
+            CreateMap<UpdateOrderDetailDto, OrderDetail>().ReverseMap();
+        }
+    }
+}
---
+++ Services/Order/Core/IEAMicroService.Services.Order.Core.Application/Mapping/OrderingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using IEAMicroService.Services.Order.Core.Application.Dtos.OrderDetailDtos;
+using IEAMicroService.Services.Order.Core.Application.Dtos.OrderDtos;
+using IEAMicroService.Services.Order.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEAMicroService.Services.Order.Core.Application.Mapping
+{
+    public class OrderingProfile : Profile
+    {
+        public OrderingProfile()
+        {
+            CreateMap<ResultOrderingDto, Ordering>().ReverseMap();
+            CreateMap<CreateOrderingDto, Ordering>().ReverseMap();
+            CreateMap<UpdateOrderingDto, Ordering>().ReverseMap();
+        }
+    }
+}
